72a58f4243e0eb019193a60578f39be98702d3c3
Charlotte Stick <charlie.stick@btinternet.com>
2024-07-31 15:39:55 +0100

    Update prettier rules and line endings

--- src/trayItem.ts
+++ src/trayItem.ts
@@ -1,4 +1,4 @@
-import { app, Menu, nativeImage, Tray } from "electron";
+import { app, Menu, nativeImage, Tray } from 'electron';
 
 export class TrayItem {
   private trayItem: Tray;
@@ -10,21 +10,21 @@ export class TrayItem {
   private createTrayItem(): void {
     // Create an item in the notification tray area so we can exit the app once the dock/taskbar item is removed
     const icon = nativeImage
-      .createFromPath("icons/jumpcut blue icon 32.png")
-      .resize({ width: 30, height: 30, quality: "best" });
+      .createFromPath('icons/jumpcut blue icon 32.png')
+      .resize({ width: 30, height: 30, quality: 'best' });
     this.trayItem = new Tray(icon);
 
     const contextMenu = Menu.buildFromTemplate([
-      { label: "Exit tscut", id: "exit", type: "normal" },
+      { label: 'Exit tscut', id: 'exit', type: 'normal' },
     ]);
 
     this.trayItem.setContextMenu(contextMenu);
-    this.trayItem.addListener("click", () => {
+    this.trayItem.addListener('click', () => {
       this.trayItem.popUpContextMenu();
     });
 
     // Menu item handlers
-    contextMenu.getMenuItemById("exit").click = () => {
+    contextMenu.getMenuItemById('exit').click = () => {
       this.trayItem.destroy();
       app.quit();
     };
