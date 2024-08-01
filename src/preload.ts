d141151dbbb085719bf65e57e37a218c0eec874b
Charlotte Stick <charlie.stick@btinternet.com>
2024-08-01 16:06:43 +0100

    Write text to bezel from main process

--- src/preload.ts
+++ src/preload.ts
@@ -1,2 +1,23 @@
-// See the Electron documentation for details on how to use preload scripts:
-// https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
+// Renderer process has no access to Node or main process modules
+// We can use this script to expose a custom API between the main process and the renderer
+import { contextBridge, ipcRenderer } from 'electron';
+
+// This type will move once the Clipping class is implemented
+export type Clipping = {
+  stackNumber: string;
+  source: string;
+  timestamp: string;
+  content: string;
+};
+
+export type ClippingsApi = {
+  onShowClipping: (handler: (clipping: Clipping) => void) => void;
+};
+
+const clippingsApi: ClippingsApi = {
+  onShowClipping: (handler) =>
+    ipcRenderer.on('showClipping', (_event, clipping) => handler(clipping)),
+};
+
+contextBridge.exposeInMainWorld('clippings', clippingsApi);
+
--- src/renderer.ts
+++ src/renderer.ts
@@ -1,34 +1,18 @@
-/**
- * This file will automatically be loaded by webpack and run in the "renderer" context.
- * To learn more about the differences between the "main" and the "renderer" context in
- * Electron, visit:
- *
- * https://electronjs.org/docs/latest/tutorial/process-model
- *
- * By default, Node.js integration in this file is disabled. When enabling Node.js integration
- * in a renderer process, please be aware of potential security implications. You can read
- * more about security risks here:
- *
- * https://electronjs.org/docs/tutorial/security
- *
- * To enable Node.js integration in this file, open up `main.js` and enable the `nodeIntegration`
- * flag:
- *
- * ```
- *  // Create the browser window.
- *  mainWindow = new BrowserWindow({
- *    width: 800,
- *    height: 600,
- *    webPreferences: {
- *      nodeIntegration: true
- *    }
- *  });
- * ```
- */
-
 import './bezel.css';
+import { ClippingsApi } from './preload';
 
-console.log(
-  '👋 This message is being logged by "renderer.js", included via webpack'
-);
+// Tell typescript that we've extended the interface with our API
+declare global {
+  interface Window {
+    clippings: ClippingsApi;
+  }
+}
 
+window.clippings.onShowClipping((clipping) => {
+  console.log('showClipping message recieved with: ' + clipping);
+  document.getElementById('stack-number').textContent = clipping.stackNumber;
+  document.getElementById('clipping-source').textContent = clipping.source;
+  document.getElementById('clipping-timestamp').textContent =
+    clipping.timestamp;
+  document.getElementById('clipping-content').textContent = clipping.content;
+});
