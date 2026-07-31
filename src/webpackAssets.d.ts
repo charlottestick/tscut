15280e158f788f05c243e1a80e8d5a082cd5c16b
Charlotte Stick <charlie.stick@btinternet.com>
2026-07-31 16:01:04 +0100

    Upgrade to Typescript 6

---
+++ src/webpackAssets.d.ts
@@ -0,0 +1,19 @@
+declare module '*.png' {
+  const value: string;
+  export default value;
+}
+
+declare module '*.ico' {
+  const value: string;
+  export default value;
+}
+
+declare module '*.css' {
+  const value: string;
+  export default value;
+}
+
+declare module '*.ttf' {
+  const value: string;
+  export default value;
+}
